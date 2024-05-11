3982de6048c4b5a60abfabbba10ba0baa68236d3
syntax019 <edubiosamuelvictor@gmail.com>
2024-05-11 01:46:56 +0100

    version 1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
+const initialItems = ["Eat pizza", "Drink water", "Fry fish"];
 function App() {
+  const [item, setItem] = useState("");
+  const [list, setList] = useState([]);
+
+  function handleAddList(items) {
+    setList((item) => [...item, items]);
+  }
+
+  function handleDeleteList(id) {
+    const updatedList = [...list];
+    updatedList.splice(id, 1);
+    setList(updatedList);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Input
+        item={item}
+        setItem={setItem}
+        setList={setList}
+        onAddList={handleAddList}
+      />
+      <ListItem list={list} onDeleteList={handleDeleteList} />
     </div>
   );
 }
 
 export default App;
+
+function Header() {
+  return <h1 class="header">Todo Input📃</h1>;
+}
+
+function Input({ item, setItem, onAddList }) {
+  function handleClick(e) {
+    e.preventDefault();
+
+    if (!item) return;
+    const newItem = { item };
+
+    onAddList(newItem);
+    setItem("");
+  }
+
+  return (
+    <div class="input">
+      <input
+        type="text"
+        value={item}
+        onChange={(e) => setItem(e.target.value)}
+        placeholder="Enter a new Task"
+      />
+      <button onClick={handleClick}>ADD TASK</button>
+    </div>
+  );
+}
+
+function ListItem({ list, onDeleteList }) {
+  return (
+    <div class="task">
+      <ul>
+        {list.map((value, id) => (
+          <NewItem value={value} id={id} onDeleteList={onDeleteList} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function NewItem({ value, id, onDeleteList }) {
+  return (
+    <div class="newItem">
+      <input type="checkbox" />
+      <li>{value.item}</li>
+      <button onClick={() => onDeleteList(id)}>❌</button>
+    </div>
+  );
+}
